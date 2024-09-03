f1cadd13d96cdf062bd486937f774fc78a2a2a90
TheDoctorOne <mahmudkocas@gmail.com>
2024-09-03 09:10:49 +0300

    Denied custom packages from C2S to disable clients' any possible interaction with 3rd party mods. Such as FTB Teams/Claims.

--- src/main/java/net/mahmutkocas/mixin/ClientConnectionMixin.java
+++ src/main/java/net/mahmutkocas/mixin/ClientConnectionMixin.java
@@ -1,10 +1,16 @@
 package net.mahmutkocas.mixin;
 
+import net.mahmutkocas.PlayerStateHandler;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.listener.PacketListener;
 import net.minecraft.network.packet.Packet;
+import net.minecraft.network.packet.c2s.play.CustomPayloadC2SPacket;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -12,11 +18,31 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ClientConnection.class)
 public class ClientConnectionMixin {
 
+    @Shadow @Final private static Logger LOGGER;
+
     @Inject(at = @At("HEAD"), method = "handlePacket", cancellable = true)
     private static <T extends PacketListener> void handlePacket(Packet<T> packet, PacketListener listener, CallbackInfo ci) {
         if(!(listener instanceof ServerPlayNetworkHandler)) {
             return;
         }
         ServerPlayNetworkHandler handler = (ServerPlayNetworkHandler) listener;
+
+        LOGGER.info("Packet class" + packet.getClass().getName());
+        if(packet instanceof CustomPayloadC2SPacket) {
+            checkLock(handler.getPlayer().getUuidAsString(), ci);
+            if(ci.isCancelled()) {
+                CustomPayloadC2SPacket pack = (CustomPayloadC2SPacket) packet;
+                LOGGER.info("Denied packet: " + pack.getChannel() + " for Player: " + handler.getPlayer().getUuidAsString());
+            }
+        }
+    }
+
+
+    @Unique
+    private static void checkLock(String uuid, CallbackInfo ci) {
+        if (!PlayerStateHandler.INSTANCE.isLocked(uuid)) {
+            return;
+        }
+        ci.cancel();
     }
 }
